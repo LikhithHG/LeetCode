022eca30c47caf4ba37d340617da39012a285c36
Likhith H G <likhith2098@gmail.com>
2024-08-22 18:21:29 -0600

    Solved Problem 345

---
+++ Strings/Two Pointers/345-Reverse_Vowels_Of_String.js
@@ -0,0 +1,31 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var reverseVowels = function(s) 
+{
+    const vowels = new Set('aeiouAEIOU');
+    
+    let arr = s.split('');
+    let low = 0;
+    let high = arr.length - 1;
+
+    while(low <= high)
+    {
+        if(!vowels.has(arr[low]))
+            low++;
+
+        else if(!vowels.has(arr[high]))
+            high--;
+
+        else
+        {
+            let char = arr[low];
+            arr[low] = arr[high];
+            arr[high] = char;
+            low++;
+            high--;
+        }
+    }
+    return arr.join('');;     
+};
