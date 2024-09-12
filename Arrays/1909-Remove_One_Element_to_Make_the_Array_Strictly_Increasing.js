26e46072bb2fef1af3fa6ec53a501ef327f79119
Likhith H G <likhith2098@gmail.com>
2024-09-12 12:37:22 -0600

    Solved Problem 1909

---
+++ Arrays/1909-Remove_One_Element_to_Make_the_Array_Strictly_Increasing.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var canBeIncreasing = function(nums) 
+{
+    let count = 0;
+
+    for (let i = 1; i < nums.length; i++) 
+    {
+        if (nums[i] <= nums[i - 1]) 
+        {
+            count++;
+            if (count > 1) 
+                return false;
+            // Check if removing nums[i] or nums[i - 1] can result in strictly increasing           sequence
+            if (i > 1 && nums[i] <= nums[i - 2] && nums[i + 1] <= nums[i - 1]) 
+            {
+                return false;
+            }
+        }
+    }
+
+    return true;   
+};
