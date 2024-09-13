281ab93c8fbcb2b3f69b14a29b0fd80d09a4b5b4
Likhith H G <likhith2098@gmail.com>
2024-09-13 14:15:21 -0600

    Solved problem 1929

---
+++ Arrays/1929-Concatenation_of_Array.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var getConcatenation = function(nums) 
+{
+    let n = nums.length;
+    let ans = new Array(2 * n);
+
+    for(let i = 0; i < n; i++)
+    {
+        ans[i] = nums[i];
+        ans[i + n] = nums[i];
+    }
+
+    return ans;
+   
+    //return nums.concat(nums);
+};
