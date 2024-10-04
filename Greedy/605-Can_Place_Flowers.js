bed389ef69409c8d8e91ae31de368d852bb4c8a6
Likhith H G <likhith2098@gmail.com>
2024-10-04 11:05:20 -0600

    Solved Problem 189

--- Greedy/605-Can_Place_Flowers.js
+++ Greedy/605-Can_Place_Flowers.js
@@ -8,29 +8,37 @@ var canPlaceFlowers = function(flowerbed, n)
     if(n === 0)
         return true;
     let i = 0;
-    while (i < flowerbed.length) {
+    while (i < flowerbed.length) 
+    {
         // Check if the current plot is empty and if adjacent plots (if any) are also empty or out of bounds
-        if (flowerbed[i] === 0) {
+        if (flowerbed[i] === 0) 
+        {
             const emptyLeft = (i === 0 || flowerbed[i - 1] === 0);
             const emptyRight = (i === flowerbed.length - 1 || flowerbed[i + 1] === 0);
             
-            if (emptyLeft && emptyRight) {
+            if (emptyLeft && emptyRight) 
+            {
                 // Plant a flower
                 flowerbed[i] = 1;
                 n--;
                 
                 // If all flowers are planted, return true
-                if (n === 0) {
+                if (n === 0) 
+                {
                     return true;
                 }
                 
                 // Skip the next plot since we just planted a flower
                 i += 2;
-            } else {
+            } 
+            else 
+            {
                 // If we can't plant a flower, move to the next plot
                 i++;
             }
-        } else {
+        } 
+        else 
+        {
             // If the plot is already occupied, move to the next plot
             i++;
         }
