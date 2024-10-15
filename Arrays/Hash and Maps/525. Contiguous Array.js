67e992acfc6ad2853aca0cf9e8a63db2357d1959
Likhith H G <likhith2098@gmail.com>
2024-10-15 14:40:38 -0600

    Solved Problem 525

---
+++ Arrays/Hash and Maps/525. Contiguous Array.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findMaxLength = function(nums) 
+{
+    let sumIndex = new Map();
+    sumIndex.set(0, -1);
+    let maxLen = 0;
+    let runningSum = 0;
+
+    for(let i = 0; i < nums.length; i++)
+    {
+        runningSum += nums[i] === 1 ? 1 : -1;
+
+        if(sumIndex.has(runningSum))
+            maxLen = Math.max(maxLen, i - sumIndex.get(runningSum));
+
+        else
+           sumIndex.set(runningSum, i); 
+    }    
+    return maxLen;
+};
