9b1d569c0d7857d790aeef7e78006d332c65811e
Likhith H G <likhith2098@gmail.com>
2024-10-16 13:26:38 -0600

    Solved Problem 75

---
+++ Arrays/Two Pointers/75. Sort Colors.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var sortColors = function(nums) 
+{
+    let low = 0;
+    let mid = 0;
+    let high = nums.length - 1;
+
+    while(mid <= high)
+    {
+        if(nums[mid] === 0)
+        {
+            [nums[mid], nums[low]] = [nums[low], nums[mid]];
+            mid += 1;
+            low += 1;
+        }
+        else if(nums[mid] === 1)
+            mid += 1;
+        else
+        {
+            [nums[mid], nums[high]] = [nums[high], nums[mid]];
+            high -= 1;
+        }
+    }
+    return nums;
+};
