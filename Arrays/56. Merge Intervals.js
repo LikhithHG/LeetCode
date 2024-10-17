e24f2e49b2a73212ab9e3978ce45b8eb4c94ed13
Likhith H G <likhith2098@gmail.com>
2024-10-17 09:38:07 -0600

    Solved Problem 56

---
+++ Arrays/56. Merge Intervals.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number[][]} intervals
+ * @return {number[][]}
+ */
+var merge = function(intervals) 
+{
+    if(intervals.length === 0)
+        return [];
+    
+    intervals.sort((a, b) => a[0] - b[0]);
+
+    const merged = [];
+    let currentInterval = intervals[0];
+
+    for(let i = 1; i < intervals.length; i++)
+    {
+        if(currentInterval[1] >= intervals[i][0])
+            currentInterval[1] = Math.max(currentInterval[1], intervals[i][1]);
+        
+        else
+        {
+            merged.push(currentInterval);
+            currentInterval = intervals[i];
+        }
+    }
+    merged.push(currentInterval);
+    return merged;
+};
