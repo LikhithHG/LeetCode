7d6d862ed158ded524103f36e4efb9b41f27c334
Likhith H G <likhith2098@gmail.com>
2024-10-18 09:52:14 -0600

    Solved Problem 1 using two pointers

--- Arrays/1-two-sum.js
+++ Arrays/1-two-sum.js
@@ -3,6 +3,25 @@
  * @param {number} target
  * @return {number[]}
  */
+var twoSum = function(nums, target) 
+{
+    let sorted = nums.map((num, index) => ({num, index})).sort((a, b) => a.num - b.num);
+    let left = 0;
+    let right = sorted.length - 1;
+
+    while(left < right)
+    {
+        let sum = sorted[left].num + sorted[right].num;
+        if(sum === target)
+            return[sorted[left].index, sorted[right].index];
+        else if(sum < target)
+            left += 1;
+        else
+            right -= 1;
+    }
+    return [];
+};
+/*
 var twoSum = function(nums, target) {
     let size = nums.length;
     for(let i = 0; i < size; i++)
@@ -15,3 +34,4 @@ var twoSum = function(nums, target) {
     }
     return []
 };
+*/
