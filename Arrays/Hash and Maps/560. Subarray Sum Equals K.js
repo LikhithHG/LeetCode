f4098d2976f6d5e27c708a2699c0c1799f4cb778
Likhith H G <likhith2098@gmail.com>
2024-10-22 17:44:31 -0600

    Solved Problem 560

--- Arrays/Hash and Maps/560. Subarray Sum Equals K.js
+++ Arrays/Hash and Maps/560. Subarray Sum Equals K.js
@@ -13,7 +13,15 @@ var subarraySum = function(nums, k)
     for(let num of nums)
     {
         sum += num;//prefix sum
-        //
+        /*
+        The prefix sum at index i is the sum of all elements in the array from the start (0) up to the current index i. Let's call this prefixSum[i]. It represents the cumulative sum of the array up to that point.
+        If we want to know if there is a subarray from some earlier index j+1 to i (i.e., subarray A[j+1] to A[i]) whose sum equals K, we can express the sum of this subarray as:
+        Subarray sum from j+1 to i = prefixSum[i]−prefixSum[j]
+
+        f the sum of the subarray from index j+1 to i is K, we get:
+        prefixSum[i] − prefixSum[j] = K
+        prefixSum[i] − K = prefixSum[j]
+        */
         if(prefixMap.has(sum - k))
             count += prefixMap.get(sum - k);
         //Update the map with the current prefix sum
