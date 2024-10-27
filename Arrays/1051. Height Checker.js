a86e4efa80fec7c8f0d4253d6d1dac7f35f9b2a9
Likhith H G <likhith2098@gmail.com>
2024-10-27 12:54:02 -0600

    Solved Problem 1051

---
+++ Arrays/1051. Height Checker.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} heights
+ * @return {number}
+ */
+var heightChecker = function(heights) 
+{
+    const expected = [...heights].sort((a, b) => a - b);
+    
+    // Count mismatches between original and sorted arrays
+    let count = 0;
+    for (let i = 0; i < heights.length; i++) 
+    {
+        if (heights[i] !== expected[i]) 
+            count++;
+    }
+    
+    return count;
+};
