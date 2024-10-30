2b93d196060e00c4a7308fdec50d695375bae9b6
Likhith H G <likhith2098@gmail.com>
2024-10-30 17:38:12 -0600

    Solved Problem 1837

---
+++ General/1837. Sum of Digits in Base K.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number} n
+ * @param {number} k
+ * @return {number}
+ */
+var sumBase = function(n, k) 
+{
+    let sum = 0;
+
+    while(n > 0)
+    {
+        sum += n % k;
+        n = Math.floor(n / k);
+    }
+    return sum;
+};
