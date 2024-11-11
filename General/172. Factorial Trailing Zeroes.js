5375641a3b817a41890ffb380613de82b37c7d1b
Likhith H G <likhith2098@gmail.com>
2024-11-11 09:29:42 -0700

    Solved Problem 172

---
+++ General/172. Factorial Trailing Zeroes.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var trailingZeroes = function(n) 
+{
+    //It depends on the factors of 10 i.e 2 and 5
+    //Always the number of factors of 2 will always be grater than that of 5 and hence it depends on factors of 5
+    let count = 0;
+    let currentDivisor = 5;
+    while(n >= currentDivisor)
+    {
+        count += Math.floor(n / currentDivisor);
+        currentDivisor *= 5;
+    }
+    return count;
+};
