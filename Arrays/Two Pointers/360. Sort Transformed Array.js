50ac1f9dca8fa0cff89184bf9a834332f48070e5
Likhith H G <likhith2098@gmail.com>
2024-12-27 10:29:08 -0700

    Solved Problem 360

---
+++ Arrays/Two Pointers/360. Sort Transformed Array.js
@@ -0,0 +1,58 @@
+/**
+ * @param {number[]} nums
+ * @param {number} a
+ * @param {number} b
+ * @param {number} c
+ * @return {number[]}
+ */
+var sortTransformedArray = function(nums, a, b, c) 
+{
+    function transform(x, a, b, c)
+    {
+        return (a * x * x) + (b * x) + c;
+    } 
+
+    let answer = [];
+    let left = 0;
+    let right = nums.length - 1;
+
+    if(a < 0) //Downward parabola
+    {
+        while(left <= right)
+        {
+            const leftVal = transform(nums[left], a, b, c);
+            const rightVal = transform(nums[right], a, b, c);
+            if(leftVal < rightVal)
+            {
+                answer.push(leftVal);
+                left++;
+            }
+            else
+            {
+                answer.push(rightVal);
+                right--;
+            }
+        }
+    }
+    else //Upward parabola or its a straight line
+    {
+        while(left <= right)
+        {
+            const leftVal = transform(nums[left], a, b, c);
+            const rightVal = transform(nums[right], a, b, c);
+            if(leftVal > rightVal)
+            {
+                answer.push(leftVal);
+                left++;
+            }
+            else
+            {
+                answer.push(rightVal);
+                right--;
+            }
+        }
+        //Reverse the decreasing array
+        answer.reverse();
+    }
+    return answer;
+};
