b874bf6307e3e207deb4815ff53d9fcd5e692b4e
Likhith H G <likhith2098@gmail.com>
2024-12-29 13:39:33 -0700

    Solved Problem 2702

---
+++ Arrays/Binary Search/2702. Minimum Operations to Make Numbers Non-positive.js
@@ -0,0 +1,37 @@
+/**
+ * @param {number[]} nums
+ * @param {number} x
+ * @param {number} y
+ * @return {number}
+ */
+var minOperations = function(nums, x, y) 
+{
+    let left = 1;
+    let right = Math.ceil(Math.max(...nums) / y);
+    let mid = right;
+  
+    const diff = x - y;
+  
+    while (left < right) 
+    {
+        mid = left + right >> 1;
+    
+        let remain = mid;
+    
+        for (let i = 0; i < nums.length; i += 1) 
+        {
+            if (nums[i] > mid * y) 
+            remain -= Math.ceil((nums[i] - mid * y) / diff);
+
+            if (remain < 0) 
+                break;
+        }
+    
+        if (remain < 0) 
+            left = mid + 1;
+        
+        else 
+            right = mid;
+    }
+    return right; 
+};
