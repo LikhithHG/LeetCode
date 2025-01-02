65d58e76aa7327bc8cc315e11618e69ef525f113
Likhith H G <likhith2098@gmail.com>
2025-01-02 10:34:46 -0700

    Solved Problem 116 using BFS Implementation

--- Trees/BFS/116. Populating Next Right Pointers in Each Node.js
+++ Trees/BFS/116. Populating Next Right Pointers in Each Node.js
@@ -37,3 +37,26 @@ var connect = function(root)
     }
     return root;
 };
+
+/*
+    //BFS
+    if(root === null)
+        return root;
+    
+    let queue = [root];
+    while(queue.length > 0)
+    {
+        let size = queue.length;
+        for(let i = 0; i < size; i++)
+        {
+            let node = queue.shift();
+            if(i < size - 1)
+                node.next = queue[0];
+            if(node.left)
+                queue.push(node.left);
+            if(node.right)
+                queue.push(node.right);
+        }
+    }
+    return root;
+*/
