54acd71368dd938ebe0291716cf43f0a2db6c9ed
Likhith H G <likhith2098@gmail.com>
2025-01-12 08:55:59 -0700

    Solved Problem 725 by creating new parts from original list

--- Lists/725. Split Linked List in Parts.js
+++ Lists/725. Split Linked List in Parts.js
@@ -1,3 +1,15 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} k
+ * @return {ListNode[]}
+ */
 var splitListToParts = function(head, k) 
 {
     //No new Parts
@@ -50,3 +62,40 @@ var splitListToParts = function(head, k)
 
     return ans;
 };
+
+/*
+    //Creating new parts
+    let ans = Array(k);
+    let size = 0;
+    let current = head;
+
+    while(current)
+    {
+        size += 1;
+        current = current.next;
+    }
+    let splitSize = Math.floor(size / k);
+    let remaining = size % k;
+    current = head;
+    for(let i = 0; i < k; i++)
+    {
+        const dummy = new ListNode(0);
+        let tail = dummy;
+        // Calculate the size of the current part
+        let currentSize = splitSize;
+        if (remaining > 0) 
+        {
+            remaining--;
+            currentSize++;
+        }
+        for (let j = 0; j < currentSize; j++) 
+        {
+            tail.next = new ListNode(current.val);
+            tail = tail.next;
+            current = current.next;
+        }
+
+        ans[i] = dummy.next;
+    }
+    return ans; 
+*/
