cfe7bd9ab9f00bbe68b01127d65a5509a14f444c
Likhith H G <likhith2098@gmail.com>
2025-01-29 12:03:11 -0700

    Worked on Allowing a function to be called once

--- Dynamic Programming/198-House_Robber.js
+++ Dynamic Programming/198-House_Robber.js
@@ -10,15 +10,21 @@ var rob = function(nums)
     if(nums.length === 1)
         return nums[0];
 
-    let dp = new Array(nums.length).fill(0);
+    // let dp = new Array(nums.length).fill(0);
 
-    dp[0] = nums[0];
-    dp[1] = Math.max(nums[0], nums[1])
+    // dp[0] = nums[0];
+    // dp[1] = Math.max(nums[0], nums[1])
+    let prev2 = 0; //dp[i - 2]
+    let prev1 = nums[0]; // dp[i - 1]
 
-    for(let i = 2; i < nums.length; i++)
+    for(let i = 1; i < nums.length; i++)
     {
-        dp[i] = Math.max(dp[i - 1], nums[i] + dp[i - 2])
+        //dp[i] = Math.max(dp[i - 1], nums[i] + dp[i - 2])
+        let current = Math.max(prev1, nums[i] + prev2);
+        prev2 = prev1;
+        prev1 = current;
     }
 
-    return dp[nums.length - 1];
+    //return dp[nums.length - 1];
+    return prev1;
 };
