d73111d50abb0893de0a85773e8084a9bcb640e3
Likhith H G <likhith2098@gmail.com>
2025-02-03 10:42:35 -0700

    Solved Problem 2637 Promise TimeLimit

---
+++ JavaScript/2637. Promise Time Limit.js
@@ -0,0 +1,34 @@
+/**
+ * @param {Function} fn
+ * @param {number} t
+ * @return {Function}
+ */
+var timeLimit = function(fn, t) 
+{
+    // return async function(...args) 
+    // {
+    //     return new Promise((resolve, reject) => {
+    //         setTimeout(() => {
+    //             reject("Time Limit Exceeded");
+    //         }, t);
+    //         fn(...args).then(resolve).catch(reject);
+    //     })
+    // }
+    return async function(...args)
+    {
+        return new Promise((resolve, reject) => {
+            const timeout = setTimeout(() => {
+                reject("Time Limit Exceeded");
+            }, t);
+            fn(...args)
+                .then(resolve)
+                .catch(reject)
+                .finally(() => clearTimeout(timeout));
+        })
+    }
+};
+
+/**
+ * const limited = timeLimit((t) => new Promise(res => setTimeout(res, t)), 100);
+ * limited(150).catch(console.log) // "Time Limit Exceeded" at t=100ms
+ */
