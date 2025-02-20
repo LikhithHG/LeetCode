8c6e87db20c852b471ac152881afe2da77851ed4
Likhith H G <likhith2098@gmail.com>
2025-02-20 12:28:26 -0700

    Leetcode 75

--- LeetCode75/605. Can Place Flowers.js
+++ LeetCode75/605. Can Place Flowers.js
@@ -5,45 +5,63 @@
  */
 var canPlaceFlowers = function(flowerbed, n) 
 {
-    if(n === 0)
-        return true;
-    let i = 0;
-    while (i < flowerbed.length) 
-    {
-        // Check if the current plot is empty and if adjacent plots (if any) are also empty or out of bounds
-        if (flowerbed[i] === 0) 
-        {
-            const emptyLeft = (i === 0 || flowerbed[i - 1] === 0);
-            const emptyRight = (i === flowerbed.length - 1 || flowerbed[i + 1] === 0);
+    // if(n === 0)
+    //     return true;
+    // let i = 0;
+    // while (i < flowerbed.length) 
+    // {
+    //     // Check if the current plot is empty and if adjacent plots (if any) are also empty or out of bounds
+    //     if (flowerbed[i] === 0) 
+    //     {
+    //         const emptyLeft = (i === 0 || flowerbed[i - 1] === 0);
+    //         const emptyRight = (i === flowerbed.length - 1 || flowerbed[i + 1] === 0);
             
-            if (emptyLeft && emptyRight) 
-            {
-                // Plant a flower
-                flowerbed[i] = 1;
-                n--;
+    //         if (emptyLeft && emptyRight) 
+    //         {
+    //             // Plant a flower
+    //             flowerbed[i] = 1;
+    //             n--;
                 
-                // If all flowers are planted, return true
-                if (n === 0) 
-                {
-                    return true;
-                }
+    //             // If all flowers are planted, return true
+    //             if (n === 0) 
+    //             {
+    //                 return true;
+    //             }
                 
-                // Skip the next plot since we just planted a flower
-                i += 2;
-            } 
-            else 
-            {
-                // If we can't plant a flower, move to the next plot
-                i++;
-            }
-        } 
-        else 
+    //             // Skip the next plot since we just planted a flower
+    //             i += 2;
+    //         } 
+    //         else 
+    //         {
+    //             // If we can't plant a flower, move to the next plot
+    //             i++;
+    //         }
+    //     } 
+    //     else 
+    //     {
+    //         // If the plot is already occupied, move to the next plot
+    //         i++;
+    //     }
+    // }
+    
+    // // If we finish the loop and haven't placed all flowers, return false
+    // return n === 0;
+
+    let count = 0;
+    for(let i = 0; i < flowerbed.length; i++)
+    {
+        if(flowerbed[i] === 0)
         {
-            // If the plot is already occupied, move to the next plot
-            i++;
+            let leftEmpty = (i === 0) || (flowerbed[i - 1] === 0);
+            let rightEmpty = (i === flowerbed.length - 1) || (flowerbed[i + 1] === 0);
+            if(leftEmpty && rightEmpty)
+            {
+                flowerbed[i] = 1;
+                count++;
+                if(count >= n)
+                    return true;
+            }
         }
     }
-    
-    // If we finish the loop and haven't placed all flowers, return false
-    return n === 0;
+    return count >= n;
 };
--- LeetCode75/746. Min Cost Climbing Stairs.js
+++ LeetCode75/746. Min Cost Climbing Stairs.js
@@ -4,18 +4,28 @@
  */
 var minCostClimbingStairs = function(cost) 
 {
-    let n = cost.length;
-    if(n === 1)
-        return cost[0];
+    // let n = cost.length;
+    // if(n === 1)
+    //     return cost[0];
 
-    let dp = new Array(n).fill(0);
+    // let dp = new Array(n).fill(0);
 
-    dp[0] = cost[0];
-    dp[1] = cost[1];
+    // dp[0] = cost[0];
+    // dp[1] = cost[1];
 
-    for(let i = 2; i < n; i++)
+    // for(let i = 2; i < n; i++)
+    // {
+    //     dp[i] = cost[i] + Math.min(dp[i - 1], dp[i - 2]);
+    // }
+    // return Math.min(dp[n - 1], dp[n - 2]);
+
+    let one  = 0;
+    let two = 0;
+    for(let i = 2; i < cost.length + 1; i++)
     {
-        dp[i] = cost[i] + Math.min(dp[i - 1], dp[i - 2]);
+        let temp = one;
+        one = Math.min(one + cost[i - 1], two + cost[i - 2]);
+        two = temp;
     }
-    return Math.min(dp[n - 1], dp[n - 2]);
+    return one;
 };
