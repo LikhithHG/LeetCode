65cbab372fe9329428646ad18d57c212608fe270
Likhith H G <likhith2098@gmail.com>
2025-04-09 22:01:36 -0600

    Resolved problem 14

--- Strings/Binary Search/14. Longest Common Prefix.js
+++ Strings/Binary Search/14. Longest Common Prefix.js
@@ -4,32 +4,33 @@
  */
 var longestCommonPrefix = function(strs) 
 {
-    if(strs.length === 0)
+     if(strs.length === 0)
         return "";
 
     let minLen = Math.min(...strs.map(str => str.length));
 
-    let low = 0;
+    let low = 0; 
     let high = minLen;
 
     while(low <= high)
     {
         let mid = Math.floor((low + high) / 2);
-        if(commonPrefix(strs, mid))
+        if(commonPrefix(mid))
             low = mid + 1;
         else
             high = mid - 1;
-
     }
     return strs[0].substring(0, high);
-};
-var commonPrefix = function(strs, len)
-{
-    const prefix = strs[0].substring(0, len);
-    for(let i = 1; i < strs.length; i++)
+
+
+    function commonPrefix(len)
     {
-        if(!strs[i].startsWith(prefix))
-            return false;
+        let prefix = strs[0].substring(0, len);
+        for(let i = 1; i < strs.length; i++)
+        {
+            if(!strs[i].startsWith(prefix))
+                return false;
+        }
+        return true;
     }
-    return true;
-}
+};
