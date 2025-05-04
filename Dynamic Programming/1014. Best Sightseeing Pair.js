2740d1e599dff5efe31b500046e2a8e7dc9813ad
Likhith H G <likhith2098@gmail.com>
2025-05-03 22:36:26 -0600

    Solved problem 1014

---
+++ Dynamic Programming/1014. Best Sightseeing Pair.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} values
+ * @return {number}
+ */
+var maxScoreSightseeingPair = function(values) 
+{
+    let n = values.length;
+
+    let maxLeftScore = new Array(n);
+    maxLeftScore[0] = values[0];
+
+    let maxScore = 0;
+
+    for(let i = 1; i < n; i++)
+    {
+        const currentRightScore = values[i] - i;
+        maxScore = Math.max(maxScore, maxLeftScore[i - 1] + currentRightScore);
+
+        let currentLeftScore = values[i] + i;
+        maxLeftScore[i] = Math.max(maxLeftScore[i - 1], currentLeftScore);
+    }
+    return maxScore;
+};
