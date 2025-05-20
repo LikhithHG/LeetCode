05a92358e66a75064b813586a855ba8bb46ead0b
Likhith H G <likhith2098@gmail.com>
2025-05-19 21:10:14 -0600

    Solved problem 309

---
+++ Dynamic Programming/309. Best Time to Buy and Sell Stock with Cooldown.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function(prices) 
+{
+    let sold = Number.MIN_SAFE_INTEGER;
+    let held = Number.MIN_SAFE_INTEGER;
+    let reset = 0;
+
+    for (let price of prices) 
+    {
+        let preSold = sold;
+
+        sold = held + price;
+        held = Math.max(held, reset - price);
+        reset = Math.max(reset, preSold);
+    }
+
+    return Math.max(sold, reset);
+};
