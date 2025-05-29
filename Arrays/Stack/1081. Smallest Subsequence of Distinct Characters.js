95b97bd89df2458698467444bf7f220a29dc5625
Likhith H G <likhith2098@gmail.com>
2025-05-28 21:20:07 -0600

    Solved problem 1081

---
+++ Arrays/Stack/1081. Smallest Subsequence of Distinct Characters.js
@@ -0,0 +1,17 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var smallestSubsequence = function(s) 
+{
+    let stack = [];
+    for(let i = 0; i < s.length; i++)
+    {
+        if(stack.includes(s[i])) 
+            continue;
+        while(stack[stack.length-1] > s[i] && s.substring(i).includes(stack[stack.length-1])) 
+            stack.pop();
+        stack.push(s[i]);
+    }
+    return stack.join("");
+};
