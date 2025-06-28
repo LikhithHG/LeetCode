68d7d02fde56135b813a6af9660704d57d82a7a8
Likhith H G <likhith2098@gmail.com>
2025-06-27 19:06:33 -0600

    Solved SQL 570

---
+++ SQL and Database/Basic Joins/570. Managers with at Least 5 Direct Reports.sql
@@ -0,0 +1,15 @@
+# Write your MySQL query statement below
+SELECT
+    name
+FROM
+    employee
+WHERE
+    id IN (
+        SELECT
+            managerId
+        FROM
+            employee
+        GROUP BY
+            managerId
+        HAVING COUNT(*) >= 5
+    );
