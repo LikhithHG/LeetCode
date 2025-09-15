f87db894e86277973742bcfd6a61fbf19d9e9411
Likhith H G <likhith2098@gmail.com>
2025-09-14 21:28:46 -0600

    Solved Problem 1650

---
+++ Trees/1650. Lowest Common Ancestor of a Binary Tree III.js
@@ -0,0 +1,47 @@
+/**
+ * // Definition for a _Node.
+ * function _Node(val) {
+ *    this.val = val;
+ *    this.left = null;
+ *    this.right = null;
+ *    this.parent = null;
+ * };
+ */
+
+/**
+ * @param {_Node} p
+ * @param {_Node} q
+ * @return {_Node}
+ */
+var lowestCommonAncestor = function(p, q) 
+{
+    let pCopy = p;
+    let qCopy = q;
+
+    while (pCopy !== qCopy) 
+    {
+        pCopy = pCopy ? pCopy.parent : q;
+        qCopy = qCopy ? qCopy.parent : p;
+    }
+
+    return pCopy;
+};
+
+/*
+    const seen = new Set();
+
+    while(p)
+    {
+        seen.add(p);
+        p = p.parent;
+    }
+
+    while(q)
+    {
+        if(seen.has(q))
+            return q;
+        q = q.parent;
+    }
+
+    return null;
+*/
