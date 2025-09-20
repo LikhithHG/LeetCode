0f9bb15f627cf9defcdc2f25b8a1ae35ff8d8716
Likhith H G <likhith2098@gmail.com>
2025-09-20 17:57:29 -0600

    Solved Problem 763 in Python

---
+++ Strings/Two Pointers/763. Partition Labels.py
@@ -0,0 +1,27 @@
+from collections import Counter 
+class Solution:
+    def partitionLabels(self, s: str) -> List[int]:
+        left = 0
+        right = 0
+        track = set()
+        count_seen = 0
+        answer = []
+
+        count = Counter(s)
+
+        while right < len(s):
+            track.add(s[right])
+            count[s[right]] -= 1
+            if count[s[right]] == 0:
+                count_seen += 1
+                if count_seen == len(track):
+                    answer.append(right - left + 1)
+                    left = right + 1
+                    right = right + 1
+                    track_set = set()
+                    count_of_seen = 0
+                    continue
+            
+            right += 1
+
+        return answer    
