4e49dd23d77ffb2760e4b771adcbc5d6a62bac5f
Likhith H G <likhith2098@gmail.com>
2025-10-23 18:32:46 -0600

    Solved Problem 2046 in python

---
+++ Lists/Two Pointers/2046. Sort Linked List Already Sorted Using Absolute Values.py
@@ -0,0 +1,22 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def sortLinkedList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev = head 
+        current = head.next
+        answer = head
+
+        while current:
+            if current.val < 0:
+                prev.next = current.next
+                current.next = answer
+                answer = current
+                current = prev.next
+            else:
+                prev = current
+                current = current.next
+        
+        return answer
