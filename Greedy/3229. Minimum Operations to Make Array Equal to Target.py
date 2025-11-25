f6fa17e031ea4bac2786d133c3da03b141adef2d
Likhith H G <likhith2098@gmail.com>
2025-11-24 17:25:24 -0700

    Solved problem 3229 in python

---
+++ Greedy/3229. Minimum Operations to Make Array Equal to Target.py
@@ -0,0 +1,26 @@
+class Solution:
+    def minimumOperations(self, nums: List[int], target: List[int]) -> int:
+        n = len(nums)
+        incr = decr = ops = 0
+
+        for i in range(n):
+            diff = target[i] - nums[i]
+
+            if diff > 0:
+                if incr < diff:
+                    ops += diff - incr
+
+                incr = diff
+                decr = 0
+
+            elif diff < 0:
+                if diff < decr:
+                    ops += decr - diff
+
+                decr = diff
+                incr = 0
+                
+            else:
+                incr = decr = 0
+
+        return ops
