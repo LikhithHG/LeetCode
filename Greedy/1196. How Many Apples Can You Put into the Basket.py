3f370d4a421b6cb560bf7274dd1452c3778c169d
Likhith H G <likhith2098@gmail.com>
2025-11-26 09:40:26 -0700

    Solved problem 1196 in python

---
+++ Greedy/1196. How Many Apples Can You Put into the Basket.py
@@ -0,0 +1,14 @@
+class Solution:
+    def maxNumberOfApples(self, weight: List[int]) -> int:
+        weight.sort()
+        apples = units = 0
+
+        for w in weight:
+            units += w
+
+            if units > 5000:
+                break
+
+            apples += 1
+            
+        return apples
