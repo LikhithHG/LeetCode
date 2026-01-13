536ab8bdc1ce0f4476d7ceeb3a7732a62a79eab3
Likhith H G <likhith2098@gmail.com>
2026-01-12 17:59:06 -0700

    Solved problem 2107 in python

---
+++ Arrays/Hash and Maps/2107. Number of Unique Flavors After Sharing K Candies.py
@@ -0,0 +1,35 @@
+class Solution:
+    def shareCandies(self, candies: List[int], k: int) -> int:
+        # Store the total number of unique flavors in the array.
+        flav_freq = defaultdict(int)
+        for c in candies:
+            flav_freq[c] += 1
+
+        # Get the total number of unique flavors in the array.
+        unique_flav = len(flav_freq)
+
+        # Get the flavors used completely in the window.
+        used_in_window = 0
+        for i in range(k):
+            flav_freq[candies[i]] -= 1
+            if flav_freq[candies[i]] == 0:
+                used_in_window += 1
+
+        # Get the flavors in the remaining array currently.
+        max_flav = unique_flav - used_in_window
+
+        # Slide the window to the right.
+        for i in range(k, len(candies)):
+            # Remove the candy on the left end from the window.
+            flav_freq[candies[i - k]] += 1
+            if flav_freq[candies[i - k]] == 1:
+                used_in_window -= 1
+
+            # Add the candy on the right end at index i.
+            flav_freq[candies[i]] -= 1
+            if flav_freq[candies[i]] == 0:
+                used_in_window += 1
+
+            max_flav = max(max_flav, unique_flav - used_in_window)
+
+        return max_flav
