944112f1b0916ae84c9eaddedebbca68770a6d08
Likhith H G <likhith2098@gmail.com>
2026-02-16 17:27:49 -0700

    Solved problem 2558 in python

---
+++ Arrays/Heaps/2558. Take Gifts From the Richest Pile.py
@@ -0,0 +1,18 @@
+class Solution:
+    def pickGifts(self, gifts: List[int], k: int) -> int:
+        # Create a max-heap from the 'gifts' array (negating values to simulate max-heap)
+        gifts_heap = [-gift for gift in gifts]
+        heapq.heapify(gifts_heap)
+
+        # Perform the operation 'k' times
+        for _ in range(k):
+            # Get the maximum element from the heap (top element)
+            max_element = -heapq.heappop(gifts_heap)
+
+            # Insert the floor of the square root of the maximum element back into the heap
+            heapq.heappush(gifts_heap, -math.isqrt(max_element))
+
+        # Accumulate the sum of the elements in the heap
+        number_of_remaining_gifts = -sum(gifts_heap)
+
+        return number_of_remaining_gifts
