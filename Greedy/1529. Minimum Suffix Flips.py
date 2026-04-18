f2491812cfce66d0ead4255b3a98b7f2d1ac578f
Likhith H G <likhith2098@gmail.com>
2026-04-17 21:46:50 -0600

    Solved problem 1529 in python

---
+++ Greedy/1529. Minimum Suffix Flips.py
@@ -0,0 +1,16 @@
+class Solution:
+    def minFlips(self, target: str) -> int:
+        if '1' not in target:
+            return 0
+
+        ok = '0'
+        ans, i, n = 0, target.find('1'), len(target)
+
+        while i < n:
+            if ok != target[i]:
+                ans += 1
+                ok = target[i]
+
+            i += 1
+            
+        return ans
