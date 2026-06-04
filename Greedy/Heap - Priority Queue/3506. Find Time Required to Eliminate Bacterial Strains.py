8fbfa23cc7fadf33f1ee6c1d0d2c4bea531b0055
Likhith H G <likhith2098@gmail.com>
2026-06-03 20:05:25 -0600

    Solved problem 3506 in python

---
+++ Greedy/Heap - Priority Queue/3506. Find Time Required to Eliminate Bacterial Strains.py
@@ -0,0 +1,10 @@
+class Solution:
+    def minEliminationTime(self, timeReq: List[int], splitTime: int) -> int:
+        heapify(timeReq)
+        heappop(timeReq)
+        
+        while timeReq:
+            bact = splitTime + heappop(timeReq)
+            heappushpop(timeReq, bact)
+
+        return bact
