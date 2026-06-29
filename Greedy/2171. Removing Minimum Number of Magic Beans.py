3f9242bfe746dfd4e1feaa61a62f1f3db84d0e1e
Likhith H G <likhith2098@gmail.com>
2026-06-28 21:28:26 -0600

    Solved problem 2171 in python

---
+++ Greedy/2171. Removing Minimum Number of Magic Beans.py
@@ -0,0 +1,8 @@
+class Solution:
+    def minimumRemoval(self, beans: List[int]) -> int:
+        mx, n = 0, len(beans)
+
+        for i, bean in enumerate(sorted(beans)):
+            mx = max(mx, bean * (n - i))
+            
+        return sum(beans) - mx
