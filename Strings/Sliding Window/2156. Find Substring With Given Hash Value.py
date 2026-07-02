fdc3c881362ee572f2976601857efa53cc9fd75c
Likhith H G <likhith2098@gmail.com>
2026-07-01 22:34:04 -0600

    Solved problem 2156 in python

---
+++ Strings/Sliding Window/2156. Find Substring With Given Hash Value.py
@@ -0,0 +1,19 @@
+class Solution:
+    def subStrHash(self, s: str, power: int, modulo: int, k: int, hashValue: int) -> str:
+        def val(c):
+            return ord(c) - ord('a') + 1
+            
+        res = n = len(s)
+        pk = pow(power, k, modulo)
+        cur = 0
+
+        for i in range(n - 1, -1, -1):
+            cur = (cur * power + val(s[i])) % modulo
+
+            if i + k < n:
+                cur = (cur - val(s[i + k]) * pk) % modulo
+
+            if cur == hashValue:
+                res = i
+
+        return s[res: res + k]
