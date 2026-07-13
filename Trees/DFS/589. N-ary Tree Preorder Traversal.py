464272f85b37f88a024b850b764605598ea40d38
Likhith H G <likhith2098@gmail.com>
2026-07-12 20:37:11 -0600

    Solved problem 589 in python

---
+++ Trees/DFS/589. N-ary Tree Preorder Traversal.py
@@ -0,0 +1,20 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val: Optional[int] = None, children: Optional[List['Node']] = None):
+        self.val = val
+        self.children = children
+"""
+
+class Solution:
+    def preorder(self, root: 'Node') -> List[int]:
+        if root is None:
+            return []
+        
+        stack, output = [root, ], []            
+        while stack:
+            root = stack.pop()
+            output.append(root.val)
+            stack.extend(root.children[::-1])
+                
+        return output
